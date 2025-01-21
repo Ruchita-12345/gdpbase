e9a8b7f9e8107c87bbb6dddb824489684ac0362a
Ruchita-12345 <138860532+Ruchita-12345@users.noreply.github.com>
2025-01-21 11:27:55 +0530

    Update setup.sh

--- Countries-GDP-prediction-master/App/setup.sh
+++ Countries-GDP-prediction-master/App/setup.sh
@@ -2,8 +2,14 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
-port = $PORT\n\
+port = \$PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+echo "\
+[python]\n\
+version = \"3.11\"\n\
+\n\
+" >> ~/.streamlit/config.toml
